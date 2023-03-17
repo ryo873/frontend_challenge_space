c4f08d0a3e789d28e6d543de2f62d374974eee3a
Ryo <ryoreinaldon11@gmail.com>
2023-03-17 14:54:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily:{
+      'barlow': ['Barlow Condensed', 'sans-serif'],
+      'bellefair': ['Bellefair', 'serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}

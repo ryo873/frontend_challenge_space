19fe46fa13c497ddd8c4ce5775ec24606ca377f5
Ryo <ryoreinaldon11@gmail.com>
2023-03-18 19:47:33 +0700

    complete mobile version for destination moon

--- dist/script.js
+++ dist/script.js
@@ -1,16 +1,18 @@
-let hamburgerButton = document.getElementById("hamburger");
-hamburgerButton.addEventListener("click", function () {
-  console.log("Aku di klik");
-  let smToogle = document.getElementById("navbarToogle");
+document.addEventListener("DOMContentLoaded", function () {
+  let hamburgerButton = document.getElementById("hamburger");
+  hamburgerButton.addEventListener("click", function () {
+    console.log("Aku di klik");
+    let smToogle = document.getElementById("navbarToogle");
 
-  smToogle.classList.add("active");
-  smToogle.classList.remove("close");
-});
+    smToogle.classList.add("active");
+    smToogle.classList.remove("close");
+  });
 
-let closeHamburger = document.getElementById("closeButton");
-closeHamburger.addEventListener("click", function () {
-  let smToogle = document.getElementById("navbarToogle");
+  let closeHamburger = document.getElementById("closeButton");
+  closeHamburger.addEventListener("click", function () {
+    let smToogle = document.getElementById("navbarToogle");
 
-  smToogle.classList.add("close");
-  smToogle.classList.remove("active");
+    smToogle.classList.add("close");
+    smToogle.classList.remove("active");
+  });
 });
